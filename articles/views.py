6f1e612adb1cdc9c67268574778908a29cc7f739
milknsoda <milknsoda9327@gmail.com>
2019-08-19 16:40:35 +0900

    04 | articles - CRUD - C

--- articles/views.py
+++ articles/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+
 
 # Create your views here.
 def index(request):
-    articles = Article.objects.all()
+    articles = Article.objects.order_by('-id')
     context = {
         'articles': articles
     }
     return render(request, 'articles/index.html', context)
 
+def new(request):
+    return render(request, 'articles/new.html')
 
+def create(request):
+    title = request.GET.get('title')
+    text = request.GET.get('text')
+    article = Article.objects.create(title=title, content=text)
+    context = {
+        'article': article
+    }
+    # return render(request, 'articles/create.html', context)
+    return redirect('/articles/')
