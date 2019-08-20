c23e65fb5e3224eeb10c9c8339964b2ad8d81463
milknsoda <milknsoda9327@gmail.com>
2019-08-20 11:21:08 +0900

    07 | articles - CRUD - U

--- articles/views.py
+++ articles/views.py
@@ -38,3 +38,18 @@ def delete(request, article_pk):
         'delete_tmp': tmp
     }
     return render(request, 'articles/delete.html', context)
+
+def edit(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/edit.html', context)
+
+def edit_result(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.GET.get('title') != article.title or request.GET.get('text') != article.content:
+        article.title = request.GET.get('title')
+        article.content = request.GET.get('text')
+        article.save()
+    return redirect(f'/articles/{article.pk}/')
