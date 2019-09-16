3a7078d263faa128c93131edec91052664dd8882
milknsoda <milknsoda9327@gmail.com>
2019-09-16 16:43:49 +0900

    13, 14 | Comment - CRUD - C, D

--- articles/urls.py
+++ articles/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('<int:article_pk>/delete/', views.delete, name='delete'),
     # path('<int:article_pk>/edit/', views.edit, name='edit'),
     path('<int:article_pk>/edit_result/', views.update, name='update'),
+    path('<int:article_pk>/comment_create/', views.comment_create, name='comment_create'),
+    path('<int:article_pk>/comment_delete/<int:comment_pk>/', views.comment_delete, name='comment_delete'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
+from django.contrib import messages
 from IPython import embed
 
-from .models import Article
-
+from .models import Article, Comment
 
 # Create your views here.
 def index(request):
@@ -42,8 +42,10 @@ def create(request):
 
 def detail(request, article_pk):
     article = Article.objects.get(pk=article_pk)
+    comment = article.comment_set.all()
     context = {
-        'article': article
+        'article': article,
+        'comments': comment
     }
     return render(request, 'articles/detail.html', context)
 
@@ -79,3 +81,16 @@ def update(request, article_pk):
         }
         return render(request, 'articles/edit.html', context)
 
+@require_POST
+def comment_create(request, article_pk):
+    content = request.POST.get('comment')
+    comment = Comment.objects.create(content=content, article_id=article_pk)
+    messages.info(request, '댓글이 등록되었습니다.')
+    return redirect('articles:detail', article_pk)
+
+@require_POST
+def comment_delete(request, article_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    comment.delete()
+    messages.success(request, '댓글이 삭제되었습니다.')
+    return redirect('articles:detail', article_pk)
